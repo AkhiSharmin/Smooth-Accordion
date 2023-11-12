ac4cd33a1695486de636bd7e0d88898ec44ba5c2
AkhiSharmin <akhisharmin100@gmail.com>
2023-11-12 12:56:14 +0600

    done Smooth Accordion

---
+++ script.js
@@ -0,0 +1,44 @@
+const faq = [
+  {
+    question: "What is React.js?",
+    answer: [
+      "React is a JavaScript library for building user interfaces.",
+      "It allows developers to create reusable UI components that update in real time as the data changes."
+    ]
+  },
+  {
+    question: "How does GraphQL differ from REST?",
+    answer: [
+      "GraphQL is a query language for APIs and a runtime for executing those queries with existing data.",
+      "Unlike REST, where the server determines the response structure, GraphQL allows clients to request only the data they need."
+    ]
+  },
+  {
+    question: "What is the purpose of a closure in JavaScript?",
+    answer: [
+      "A closure is a function bundled together with references to its surrounding state.",
+      "It allows the function to access variables from its lexical scope even when the function is executed outside that scope."
+    ]
+  },
+  {
+    question: "Why is asynchronous programming important?",
+    answer: [
+      "Asynchronous programming allows non-blocking execution of code.",
+      "This is crucial for handling tasks such as fetching data from a server, where waiting for the data to be received would freeze the application."
+    ]
+  },
+  {
+    question: "What is the difference between 'let', 'const', and 'var' in JavaScript?",
+    answer: [
+      "'var' is function-scoped, 'let' is block-scoped, and 'const' is block-scoped for variables that won't be reassigned.",
+      "Using 'const' and 'let' helps prevent bugs related to variable reassignment and scope."
+    ]
+  },
+  {
+    question: "What is the role of the Document Object Model (DOM) in web development?",
+    answer: [
+      "The DOM is a programming interface for web documents.",
+      "It represents the structure of a document as a tree of objects, allowing dynamic access and modification of the document's content, structure, and style."
+    ]
+  }
+];
